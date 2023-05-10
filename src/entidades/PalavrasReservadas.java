04625e6e0099a7ffee66c3cc959aa9950a1b0d1b
RuiYuriAfricano <yuriafricano03@gmail.com>
2023-05-10 20:34:19 +0100

    nova-versao-do-lexico-corrigindo-erro-textual-autor

--- src/entidades/PalavrasReservadas.java
+++ src/entidades/PalavrasReservadas.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author Total Energies
+ * @author Rui Malemba
  */
 public class PalavrasReservadas {
     public static ArrayList<String> reservedWords;
